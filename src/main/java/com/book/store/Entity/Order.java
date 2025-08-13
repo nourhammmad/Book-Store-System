4a04a220a92377805851f9c75a6ffd78c8d05235
Nour Hammad <nour.hammad@vodafone.com>
2025-08-13 22:29:49 +0300

    User structure:User, Admin and Customer

--- src/main/java/com/book/store/Entity/Order.java
+++ src/main/java/com/book/store/Entity/Order.java
@@ -2,6 +2,7 @@ package com.book.store.Entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,19 +19,17 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @CreationTimestamp
     private LocalDateTime orderDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id", nullable = false)
-    private Customer customer;
+    private User customer;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items = new ArrayList<>();
 
     private float totalPrice;
 
-    @PrePersist
-    protected void onCreate() {
-        orderDate = LocalDateTime.now();
-    }
+
 }
