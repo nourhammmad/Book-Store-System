6f13f503b2c1be178e1a2a5cdc9814e17c1be603
ShahdRamzy <shahd.ramzy123@gmail.com>
2025-08-26 17:40:21 +0300

    ref: renamed path names to start with the role

--- src/main/java/com/book/store/controller/CustomerController.java
+++ src/main/java/com/book/store/controller/CustomerController.java
@@ -19,20 +19,21 @@ public class CustomerController implements CustomersApi {
     private final CustomerService customerService;
     private final CustomerMapper customerMapper;
 
+
     @Override
-    public ResponseEntity<PaginatedCustomerResponseApiDto> customerGet(Integer page, Integer size) {
+    public ResponseEntity<PaginatedCustomerResponseApiDto> adminCustomerGet(Integer page, Integer size) {
         PaginatedCustomerResponseApiDto customerDtos = customerService.getAllCustomers(page, size);
         return ResponseEntity.ok(customerDtos);
     }
 
     @Override
-    public ResponseEntity<Void> customerIdDelete(Long id) {
+    public ResponseEntity<Void> adminCustomerIdDelete(Long id) {
         customerService.deleteCustomer(id);
         return ResponseEntity.noContent().build();
     }
 
     @Override
-    public ResponseEntity<CustomerApiDto> customerIdGet(Long id) {
+    public ResponseEntity<CustomerApiDto> adminCustomerIdGet(Long id) {
         Customer customer = customerService.findCustomerById(id);
         CustomerApiDto dto = customerMapper.toDTO(customer);
         return ResponseEntity.ok(dto);
